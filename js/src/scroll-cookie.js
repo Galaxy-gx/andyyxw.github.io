eff77400411e19c9d6d3cd878251d1d3044ba0db
AndyGodLike <andyyxw@163.com>
2018-10-15 19:39:56 +0800

    blog update

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Oct 15 2018 19:12:03 GMT+0800 (中国标准时间)
+// build time:Mon Oct 15 2018 19:39:45 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
